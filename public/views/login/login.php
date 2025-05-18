16b308a0c4313c009c98d6a96f2718bea921ca24
gema_handika <gemahandika88@gmail.com>
2025-05-18 23:39:49 +0700

    update login

--- public/views/login/login.php
+++ public/views/login/login.php
@@ -91,9 +91,7 @@ if (isset($_POST['login'])) {
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="../../../app/assets/img/favicon.png" rel="icon">
-    <link href="../../../app/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
+    <link href="../../../app/assets/img/JNE.png" rel="icon">
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
@@ -106,17 +104,7 @@ if (isset($_POST['login'])) {
     <link href="../../../app/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
     <link href="../../../app/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
     <link href="../../../app/assets/vendor/simple-datatables/style.css" rel="stylesheet">
-
-    <!-- Template Main CSS File -->
     <link href="../../../app/assets/css/style.css" rel="stylesheet">
-
-    <!-- =======================================================
-  * Template Name: NiceAdmin
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Updated: Apr 20 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>
@@ -130,16 +118,11 @@ if (isset($_POST['login'])) {
                         <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
                             <div class="d-flex justify-content-center py-4">
-                                <a href="index.html" class="logo d-flex align-items-center w-auto">
-                                    <img src="../../../app/assets/img/logo.png" alt="">
-                                    <span class="d-none d-lg-block">E - VOUCHER</span>
-                                </a>
-                            </div><!-- End Logo -->
-
+                                <img src="../../../app/assets/img/JNE.png" alt="JNE Logo" width="90">
+                                <span class="d-none d-lg-block fw-bold ms-3" style="font-size: 1.5rem; color: #012970;">E - ATK</span>
+                            </div>
                             <div class="card mb-3">
-
                                 <div class="card-body">
-
                                     <div class="pt-4 pb-2">
                                         <h5 class="card-title text-center pb-0 fs-4">Login Account</h5>
                                         <p class="text-center small text-danger">
@@ -156,8 +139,7 @@ if (isset($_POST['login'])) {
                                         <div class="col-12">
                                             <label for="yourUsername" class="form-label">Username</label>
                                             <div class="input-group has-validation">
-                                                <!-- <span class="input-group-text" id="inputGroupPrepend">@</span> -->
-                                                <input type="text" name="username" class="form-control" id="yourUsername" required>
+                                                <input type="text" name="username" class="form-control" id="yourUsername" value="<?php echo htmlspecialchars($username, ENT_QUOTES, 'UTF-8') ?>" autofocus>
                                                 <div class="invalid-feedback">Please enter your username.</div>
                                             </div>
                                         </div>
